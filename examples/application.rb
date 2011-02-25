fe69b2941f8434116f82cd7cf577592c66cfed51
Gimi Liang <liang.gimi@gmail.com>
2011-02-25 16:28:19 +0800

    move codes to where they should belong to.

--- examples/application.rb
+++ examples/application.rb
@@ -6,11 +6,9 @@ Bundler.setup
 
 $:.unshift File.expand_path('..', __FILE__)
 
-require 'mustache'
-require 'http_router'
 require 'thin'
 
-module Scopion
+module Hejia
   module Model
     require 'mongoid'
 
@@ -21,38 +19,6 @@ module Scopion
     %w[list show form create update].each { |res|
       require "resources/post/#{res}"
     }
-  end
-
-  class HaltedResponse
-  end
-
-  class Resource < Mustache
-
-    class << self
-      def call(env)
-        new(env).process
-      end
-    end
-
-    attr_reader :env
-
-    def initialize(env)
-      @env = env
-    end
-
-    private
-
-    def process
-      result = catch(:halt) { call }
-      reulst.is_a?(HaltedResponse) && result.to_a || render
-    end
-
-    # Subclass can rewrite this method to implement its logic.
-    def call; end
-
-    def request
-      @request ||= Rack::Request.new env
-    end
   end
 end
 
--- lib/scopion.rb
+++ lib/scopion.rb
@@ -1,5 +1,39 @@
 # encoding: utf-8
 # @auther 梁智敏 gimi<dot>liang<at>gmail<dot>com
 
+require 'mustache'
+require 'http_router'
+
 module Scopion
+
+  class HaltedResponse; end
+
+  class Resource < Mustache
+
+    class << self
+      def call(env)
+        new(env).process
+      end
+    end
+
+    attr_reader :env
+
+    def initialize(env)
+      @env = env
+    end
+
+    private
+
+    def process
+      result = catch(:halt) { call }
+      reulst.is_a?(HaltedResponse) && result.to_a || render
+    end
+
+    # Subclass can rewrite this method to implement its logic.
+    def call; end
+
+    def request
+      @request ||= Rack::Request.new env
+    end
+  end
 end
